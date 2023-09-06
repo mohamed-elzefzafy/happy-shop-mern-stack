54627e9f20c5c5c08dd42ab1396fafd3b9b0dfaa
mzefzafy <mohamedelzefzafy@gmail.com>
2023-09-06 21:34:15 +0300

    6-9-2023 9pm

---
+++ ecommerce-frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import NavBarLogin from "../../components/utilities/NavBarLogin"
+import Slider from "../../components/home/Slider"
+
+
+const HomePage = () => {
+  return (
+    <div className="font">
+        <NavBarLogin/>
+        <Slider/>
+    </div>
+  )
+}
+
+export default HomePage
